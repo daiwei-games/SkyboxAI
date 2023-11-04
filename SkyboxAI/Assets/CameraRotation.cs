83dc9769a6f4304160a9873b6ef0e5785554cda2
daiwei-games <98375826+daiwei-games@users.noreply.github.com>
2023-11-04 17:42:20 +0800

    場景演示
    攝影機前後移動、鏡頭隨滑鼠旋轉

---
+++ SkyboxAI/Assets/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float positionSpeed;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * positionSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * positionSpeed;
+        }
+        transform.Rotate(Vector3.up, mouseX);
+        transform.Rotate(Vector3.left, mouseY);
+    }
+}
---
+++ SkyboxAI/Assets/SkyboxScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SkyboxScript : MonoBehaviour
+{
+    public Material skyboxMaterial; // 這裡你可以拖曳你的材質球到Inspector面板中
+    public Texture[] panoramaTextures; // 這裡你可以拖曳你的全景圖紋理陣列到Inspector面板中
+
+    void Start()
+    {
+        // 初始時設置材質球的主紋理
+        skyboxMaterial.mainTexture = panoramaTextures[0]; // 默認使用陣列中的第一張全景圖
+
+        // 如果你想在遊戲運行時動態更換紋理，可以呼叫下面的方法
+        // ChangePanoramaTexture(1); // 傳入陣列中的索引來更換紋理
+    }
+
+    // 你可以在腳本的任何地方呼叫這個方法來更改材質球的主紋理
+    public void ChangePanoramaTexture(int textureIndex)
+    {
+        if (textureIndex >= 0 && textureIndex < panoramaTextures.Length)
+        {
+            skyboxMaterial.mainTexture = panoramaTextures[textureIndex];
+        }
+        else
+        {
+            Debug.LogError("無效的紋理索引！");
+        }
+    }
+}
